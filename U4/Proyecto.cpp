46345ad0a3f3727829d0ef709806d41165fddcf7
UP210752 <up210752@alumnos.upa.edu.mx>
2022-12-08 13:51:59 -0600

    GATO FINAL REVISION

--- U4/Proyecto.cpp
+++ U4/Proyecto.cpp
@@ -1,114 +1,115 @@
 #include <stdio.h>
 #include <iostream>
-#include <limits>
-#include <cstdio>
-#include <limits.h>
-#include <string>
-int main(){
-	
-	char matriz[3][3], opc;
-	int i, j;
-	
-	printf("Juego del gato!\n");
-	
-	
-	for(i=0; i<3; i++){
-		for(j=0; j<3; j++){
-			matriz[i][j]=' ';
-			printf("[%c]", matriz[i][j]);
-		}
-		printf("\n");
-	}
-	
-	printf("\n\nPresione Y para jugar / N para salir: ");
-	scanf("%c", &opc);
-	
-	int fila, col, ganador=0, turno=1;
-	
-	if(opc == 'Y' || opc == 'y'){
-		
-		do{
-			if(turno%2==1){
-				do{
-					printf("\nJugador 1: \n");
-					printf("Digite fila: ");
-					scanf("%d", &fila);
-					printf("Digite columna: ");
-					scanf("%d", &col);
-					
-					if(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2){
-						printf("\nCoordenadas no validas, pruebe otra vez.\n");
-					}
-				}while(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2);
-				
-				matriz[fila][col]='x';
-				
-				system("clean");
-				for(i=0; i<3; i++){
-					for(j=0; j<3; j++){
-						printf("[%c]", matriz[i][j]);
-					}
-					printf("\n");
-				}
-				turno++;
-			} else if(turno%2==0){
-				do{
-					printf("\nJugador 2: \n");
-					printf("Digite fila: ");
-					scanf("%d", &fila);
-					printf("Digite columna: ");
-					scanf("%d", &col);
-					
-					if(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2){
-						printf("\nCoordenadas no validas, pruebe otra vez.\n");
-					}
-				} while(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2);
-				
-				matriz[fila][col]='o';
-				
-				system("clear");
-				for(i=0; i<3; i++){
-					for(j=0; j<3; j++){
-						printf("[%c]", matriz[i][j]);
-					}
-					printf("\n");
-				}
-				turno++;
-			}
-			//posiciones ganadores de jugador 1
-			if(matriz[0][0] == 'x' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]
-			|| matriz[1][0] == 'x' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2]
-			|| matriz[2][0] == 'x' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
-			
-			|| matriz[0][0] == 'x' && matriz[0][0] == matriz[1][0] && matriz[0][0] == matriz[2][0]
-			|| matriz[0][1] == 'x' && matriz[0][1] == matriz[1][1] && matriz[0][1] == matriz[2][1]
-			|| matriz[0][2] == 'x' && matriz[0][2] == matriz[1][2] && matriz[0][2] == matriz[2][2]
-			
-			|| matriz[0][0] == 'x' && matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2]
-			|| matriz[0][2] == 'x' && matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0]){
-				ganador=1;
-				printf("\nGano el jugador 1\n");
-			}
-			//posiciones ganadores de jugador 2
-			if(matriz[0][0] == 'o' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]
-			|| matriz[1][0] == 'o' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2]
-			|| matriz[2][0] == 'o' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
-			
-			|| matriz[0][0] == 'o' && matriz[0][0] == matriz[1][0] && matriz[0][0] == matriz[2][0]
-			|| matriz[0][1] == 'o' && matriz[0][1] == matriz[1][1] && matriz[0][1] == matriz[2][1]
-			|| matriz[0][2] == 'o' && matriz[0][2] == matriz[1][2] && matriz[0][2] == matriz[2][2]
-			
-			|| matriz[0][0] == 'o' && matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2]
-			|| matriz[0][2] == 'o' && matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0]){
-				ganador=1;
-				printf("\nGano el jugador 2\n");
-			}
-			
-		} while(ganador != 1);
-	} else {
-		system("clear");
-		printf("\nVuelva a ejecutar el programa\n");
-	}
-	
-	return 0;
+using namespace std;
+
+int ganadorx(char matriz[3][3], int ganador=0){
+    if (matriz[0][0] == 'x' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2] || matriz[1][0] == 'x' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2] || matriz[2][0] == 'x' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
+
+            || matriz[0][0] == 'x' && matriz[0][0] == matriz[1][0] && matriz[0][0] == matriz[2][0] || matriz[0][1] == 'x' && matriz[0][1] == matriz[1][1] && matriz[0][1] == matriz[2][1] || matriz[0][2] == 'x' && matriz[0][2] == matriz[1][2] && matriz[0][2] == matriz[2][2]
+
+            || matriz[0][0] == 'x' && matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2] || matriz[0][2] == 'x' && matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0])
+        {
+            ganador = 1;
+            cout << "Felicidades jugador X" << endl;
+            exit(0);
+        }
+        return ganador;
 }
+int ganadoro(char matriz[3][3], int ganador=0){
+    if (matriz[0][0] == 'o' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2] || matriz[1][0] == 'o' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2] || matriz[2][0] == 'o' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
+
+            || matriz[0][0] == 'o' && matriz[0][0] == matriz[1][0] && matriz[0][0] == matriz[2][0] || matriz[0][1] == 'o' && matriz[0][1] == matriz[1][1] && matriz[0][1] == matriz[2][1] || matriz[0][2] == 'o' && matriz[0][2] == matriz[1][2] && matriz[0][2] == matriz[2][2]
+
+            || matriz[0][0] == 'o' && matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2] || matriz[0][2] == 'o' && matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0])
+        {
+            ganador = 1;
+            cout << "Felicidades jugador O" << endl;
+            exit(0);
+        }
+    return ganador;
+}
+int main()
+{
+
+    char matriz[3][3];
+    printf("Juego del gato!\n");
+    int contador=0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            matriz[i][j] = ' ';
+            printf("{%c}", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    int fila, columna, ganador = 0, turno = 1;
+
+    do
+    {
+        if (turno % 2 == 1)
+        {
+            do
+            {
+                cout << "Jugador X:" << endl;
+                cout <<endl<<"FILA" << endl;
+                scanf("%d", &fila);
+                cout <<endl<<"COLUMNA" << endl;
+                scanf("%d", &columna);
+                if (matriz[fila][columna] == 'x' || matriz[fila][columna] == 'o' || columna > 2 || fila > 2)
+                {
+                    cout << endl;
+                }
+            } while (matriz[fila][columna] == 'x' || matriz[fila][columna] == 'o' || columna > 2 || fila > 2);
+            matriz[fila][columna] = 'x';
+            system("clean");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    printf("{%c}", matriz[i][j]);
+                }
+                printf("\n");
+            }
+            turno++;
+        }
+        else if (turno % 2 == 0)
+        {
+            do
+            {
+                cout << "Jugador O" << endl;
+                cout <<endl<<"FILA" << endl;
+                scanf("%d", &fila);
+                cout <<endl<<"COLUMNA" << endl;
+                scanf("%d", &columna);
+
+                if (matriz[fila][columna] == 'x' || matriz[fila][columna] == 'o' || columna > 2 || fila > 2)
+                {
+                    cout <<endl<<"Ingresa otro valor";
+                }
+            } while (matriz[fila][columna] == 'x' || matriz[fila][columna] == 'o' || columna > 2 || fila > 2);
+
+            matriz[fila][columna] = 'o';
+
+            system("clear");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    printf("{%c}", matriz[i][j]);
+                }
+                cout << endl;
+            }
+            turno++;
+        }
+        // posiciones ganadores de jugador 1
+        int gx,go;
+        gx=ganadorx(matriz, ganador);
+        // posiciones ganadores de jugador 2
+        go=ganadoro(matriz, ganador);
+    } while (ganador < 1);
+    return 0;
+}
+
--- U4/README.md
+++ U4/README.md
@@ -1,122 +1,8 @@
-* UNIT 4 PORTFOLIO EVIDENCE
+# UNIT 4 PORTFOLIO EVIDENCE
 <center>
-<b>Tic Tac Toe</b>
+<H1>Tic Tac Toe</H1>
 </center>
-<br>
 [The user can play tic tac toe vs another player]
-<pre>
-int main(){
-	
-	char matriz[3][3], opc;
-	int i, j;
-	
-	printf("Juego del gato!\n");
-	
-	
-	for(i=0; i<3; i++){
-		for(j=0; j<3; j++){
-			matriz[i][j]=' ';
-			printf("[%c]", matriz[i][j]);
-		}
-		printf("\n");
-	}
-	
-	printf("\n\nPresione Y para jugar / N para salir: ");
-	scanf("%c", &opc);
-	
-	int fila, col, ganador=0, turno=1;
-	
-	if(opc == 'Y' || opc == 'y'){
-		
-		do{
-			if(turno%2==1){
-				do{
-					printf("\nJugador 1: \n");
-					printf("Digite fila: ");
-					scanf("%d", &fila);
-					printf("Digite columna: ");
-					scanf("%d", &col);
-					
-					if(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2){
-						printf("\nCoordenadas no validas, pruebe otra vez.\n");
-					}
-				}while(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2);
-				
-				matriz[fila][col]='x';
-				
-				system("clean");
-				for(i=0; i<3; i++){
-					for(j=0; j<3; j++){
-						printf("[%c]", matriz[i][j]);
-					}
-					printf("\n");
-				}
-				turno++;
-			} else if(turno%2==0){
-				do{
-					printf("\nJugador 2: \n");
-					printf("Digite fila: ");
-					scanf("%d", &fila);
-					printf("Digite columna: ");
-					scanf("%d", &col);
-					
-					if(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2){
-						printf("\nCoordenadas no validas, pruebe otra vez.\n");
-					}
-				} while(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2);
-				
-				matriz[fila][col]='o';
-				
-				system("clear");
-				for(i=0; i<3; i++){
-					for(j=0; j<3; j++){
-						printf("[%c]", matriz[i][j]);
-					}
-					printf("\n");
-				}
-				turno++;
-			}
-			//posiciones ganadores de jugador 1
-			if(matriz[0][0] == 'x' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]
-			|| matriz[1][0] == 'x' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2]
-			|| matriz[2][0] == 'x' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
-			
-			|| matriz[0][0] == 'x' && matriz[0][0] == matriz[1][0] && matriz[0][0] == matriz[2][0]
-			|| matriz[0][1] == 'x' && matriz[0][1] == matriz[1][1] && matriz[0][1] == matriz[2][1]
-			|| matriz[0][2] == 'x' && matriz[0][2] == matriz[1][2] && matriz[0][2] == matriz[2][2]
-			
-			|| matriz[0][0] == 'x' && matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2]
-			|| matriz[0][2] == 'x' && matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0]){
-				ganador=1;
-				printf("\nGano el jugador 1\n");
-			}
-			//posiciones ganadores de jugador 2
-			if(matriz[0][0] == 'o' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]
-			|| matriz[1][0] == 'o' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2]
-			|| matriz[2][0] == 'o' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
-			
-			|| matriz[0][0] == 'o' && matriz[0][0] == matriz[1][0] && matriz[0][0] == matriz[2][0]
-			|| matriz[0][1] == 'o' && matriz[0][1] == matriz[1][1] && matriz[0][1] == matriz[2][1]
-			|| matriz[0][2] == 'o' && matriz[0][2] == matriz[1][2] && matriz[0][2] == matriz[2][2]
-			
-			|| matriz[0][0] == 'o' && matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2]
-			|| matriz[0][2] == 'o' && matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0]){
-				ganador=1;
-				printf("\nGano el jugador 2\n");
-			}
-			
-		} while(ganador != 1);
-	} else {
-		system("clear");
-		printf("\nVuelva a ejecutar el programa\n");
-	}
-	
-	return 0;
-}
-</pre><br>
-
----
-
 <center>
 <b>Print:</b>
 </center>
@@ -129,4 +15,11 @@ int main(){
 <center>
 <b>Flowchart:</b>
 </center>
-<img src="img/diagrama.jpeg" alt="diagrama">
+<img src="img/diagramaF.jpeg" alt="diagrama">
+
+---
+
+<center>
+<b>Functions:</b>
+</center>
+<img src="img/funcion.jpeg" alt="funcion">
